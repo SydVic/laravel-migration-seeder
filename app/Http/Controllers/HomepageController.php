bb19565c9f6dff041726e97a744dc9a0759f2f1d
SydVic <fabrizio.bocca27@gmail.com>
2022-06-29 16:11:18 +0200

    creato controller per view e richiamato database

---
+++ app/Http/Controllers/HomepageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Train;
+use Illuminate\Http\Request;
+
+class HomepageController extends Controller
+{
+    public function index() {
+        $trains = Train::all();
+        dd($trains);
+        return view('home');
+    }
+}
